6bff5853f7f0e44c6bb4c3cd0755203f0dd99df0
Riyadh Al Nur <riyadhalnur@verticalaxisbd.com>
2015-09-19 20:56:42 +0600

    Add API key and parse returned data properly

--- index.js
+++ index.js
@@ -6,15 +6,26 @@ var _ = require('lodash');
 module.exports = function (opts, callback) {
 	opts = opts || '';
 
-	var requestURL = (opts === 'qotd') ? 'https://favqs.com/api/qotd.json' : 'https://favqs.com/api/quotes' + _.random(0, 9999);
+	var requestURL = (opts === 'qotd') ? 'https://favqs.com/api/qotd.json' : 'https://favqs.com/api/quotes/';
 
-	got(requestURL, function (err, data, response) {
+	var headers = {
+		'Authorization': 'Token token=a857a430e3a1e6d481eaafc1ab6e1f19'
+	};
+
+	got(requestURL, {headers: headers}, function (err, data, response) {
 		if (err) {
 			return callback(err);
 		}
 
 		if (response.statusCode === 200 && data) {
-			return callback(null, JSON.parse(data));
+			var parsedData = JSON.parse(data);
+
+			if (parsedData.quotes && _.isArray(parsedData.quotes)) {
+				var result = {quote: _.sample(parsedData.quotes)};
+				callback(null, result);
+			} else {
+				callback(null, parsedData);
+			}
 		}
 	});
 };
